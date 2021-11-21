06cc90529f63f6b3cf541c382d3996a20c9ca63d
Katia-Radchenko <82673991+Katia-Radchenko@users.noreply.github.com>
2021-11-21 16:12:34 +0200

    доделала

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const createitem = galleryItems
+  .map(({ preview, original, description }) =>
+      
+    `<a class="gallery__item" href="${original}"
+      style=' height: 240px'>
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`,
+  )
+    .join('');
+galleryContainer.insertAdjacentHTML('beforeend', createitem);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: `250`,
+});
