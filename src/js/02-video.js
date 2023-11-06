8a01b83d8d28168f8a7454731cdd657b06814c3c
KaterynaRadchenko <82673991+Katia-Radchenko@users.noreply.github.com>
2023-11-06 14:40:18 +0200

    HW

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const localStorageKey = 'videoplayer-current-time';
+
+
+function onTimeUpDate(evt) {
+  localStorage.setItem(localStorageKey, evt.seconds);
+}
+
+const saveTimeLocalSt = localStorage.getItem(localStorageKey);
+console.log(saveTimeLocalSt);
+if (saveTimeLocalSt) {
+  player.setCurrentTime(saveTimeLocalSt);
+}
+player.on('timeupdate', throttle(onTimeUpDate, 1000));
+
 
